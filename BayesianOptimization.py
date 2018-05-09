9eb2344eb9eca20410ac0f9c1abed15fdee5f350
Lili <lilimeng1103@gmail.com>
2018-05-09 16:27:26 -0700

    fix bugs

--- BayesianOptimization.py
+++ BayesianOptimization.py
@@ -2,31 +2,25 @@ import sklearn.gaussian_process as gp
 import cv2
 import numpy as np
 
-def superpixel_IOU():
-    img1 = cv2.imread('masks/mask_47_1.png',0)
-    img2 = cv2.imread('masks/mask_48_1.png',0)
+def superpixel_IOU(img1, img2):
 
-    print(img1.shape)
-    
     n = img1.shape[0]
     white_intersect_count = 0
-    black_intersect_count = 0
     for i in range(n):
       for j in range(n):
         if img1[i][j]==img2[i][j]==255:
           white_intersect_count+=1
-        if img1[i][j]==img2[i][j]==0:
-          black_intersect_count+=1
 
-    print("white_intersect_count: ", white_intersect_count)
-    print("white_intersect_count_percent: ", white_intersect_count*100/(n*n))
-    print("black_intersect_count: ", black_intersect_count)
-    print("black_intersect_count_percent: ", black_intersect_count*100/(n*n))
-
-
+    white_intersect_count_percent = white_intersect_count*100/(n*n)
+    
+    print("white_intersect_count_percent: ", white_intersect_count_percent)
+    return white_intersect_count_percent
 
 def main():
-    superpixel_IOU()
+
+    img1 = cv2.imread('masks/mask_47_1.png',0)
+    img2 = cv2.imread('masks/mask_48_1.png',0)
+    superpixel_IOU(img1, img2)
 
 if __name__== "__main__":
   main()
