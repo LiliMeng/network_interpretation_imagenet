00673da9e1d55519bc8f1378eb054036e8be5ed4
Lili <lilimeng1103@gmail.com>
2018-05-14 21:46:49 -0700

    fix bugs

--- BayesianOptimization.py
+++ BayesianOptimization.py
@@ -215,9 +215,7 @@ def Jaccard_distance(img1, img2):
 
 
 class JaccardDistRBF():
-    """Same as sklearn.gaussian_process.kernels.RBF except that
-    it allows for a custom distance function in the RBF kernel.
-    more details: https://stackoverflow.com/questions/49763064/haversine-distance-in-sklearn-gaussian-process-kernels?rq=1
+    """Radial-basis function kernel (aka squared-exponential kernel).
     The RBF kernel is a stationary kernel. It is also known as the
     "squared exponential" kernel. It is parameterized by a length-scale
     parameter length_scale>0, which can either be a scalar (isotropic variant
@@ -227,7 +225,7 @@ class JaccardDistRBF():
     This kernel is infinitely differentiable, which implies that GPs with this
     kernel as covariance function have mean square derivatives of all orders,
     and are thus very smooth.
-
+    .. versionadded:: 0.18
     Parameters
     -----------
     length_scale : float or array with shape (n_features,), default: 1.0
@@ -236,37 +234,49 @@ class JaccardDistRBF():
         of l defines the length-scale of the respective feature dimension.
     length_scale_bounds : pair of floats >= 0, default: (1e-5, 1e5)
         The lower and upper bound on length_scale
-
     """
-    def __init__(self, length_scale = 1.0, length_scale_bounds = (1e-5, 1e5)):#, dist_func=Jaccard_distance):
+    def __init__(self, length_scale=1.0, length_scale_bounds=(1e-5, 1e5)):
         self.length_scale = length_scale
         self.length_scale_bounds = length_scale_bounds
-        #self.dist_func = Jaccard_distance
 
-    def __call__(self, X, Y=None, eval_graident=False):
+    @property
+    def anisotropic(self):
+        return np.iterable(self.length_scale) and len(self.length_scale) > 1
+
+    @property
+    def hyperparameter_length_scale(self):
+        if self.anisotropic:
+            return Hyperparameter("length_scale", "numeric",
+                                  self.length_scale_bounds,
+                                  len(self.length_scale))
+        return Hyperparameter(
+            "length_scale", "numeric", self.length_scale_bounds)
+
+    def __call__(self, X, Y=None, eval_gradient=False):
         """Return the kernel k(X, Y) and optionally its gradient.
         Parameters
-        -----------
-        X: array, shape (n_samples_X, n_features)
+        ----------
+        X : array, shape (n_samples_X, n_features)
             Left argument of the returned kernel k(X, Y)
-        Y: array, shape (n_samples_Y, n_features), (optional, default=None)
-            Right argument of the returned kernel k(X, Y). If None, k(X,X)
+        Y : array, shape (n_samples_Y, n_features), (optional, default=None)
+            Right argument of the returned kernel k(X, Y). If None, k(X, X)
             if evaluated instead.
-        eval_gradient : bool (optional, default = False)
+        eval_gradient : bool (optional, default=False)
             Determines whether the gradient with respect to the kernel
             hyperparameter is determined. Only supported when Y is None.
-        Returns:
-        --------
-        K: array, shape (n_samples_X, n_samples_Y)
+        Returns
+        -------
+        K : array, shape (n_samples_X, n_samples_Y)
             Kernel k(X, Y)
-        K_gradient: array (opt.), shape (n_samples_x, n_samples_X, n_dims)
-            The gradient of the kernel k(X, X) with respect to the hyperparameter
-            of the kernel. Only returned when eval_gradient is True.
+        K_gradient : array (opt.), shape (n_samples_X, n_samples_X, n_dims)
+            The gradient of the kernel k(X, X) with respect to the
+            hyperparameter of the kernel. Only returned when eval_gradient
+            is True.
         """
         X = np.atleast_2d(X)
         length_scale = _check_length_scale(X, self.length_scale)
         if Y is None:
-            dists = pdist(X / length_scale, metric = 'sqeuclidean')
+            dists = pdist(X / length_scale, metric='sqeuclidean')
             K = np.exp(-.5 * dists)
             # convert from upper-triangular matrix to square matrix
             K = squareform(K)
@@ -276,7 +286,7 @@ class JaccardDistRBF():
                 raise ValueError(
                     "Gradient can only be evaluated when Y is None.")
             #dists = cdist(X / length_scale, Y / length_scale,
-             #S             metric='sqeuclidean')
+            #              metric='sqeuclidean')
             dists = Jaccard_distance(X, Y)
             K = np.exp(-.5 * dists)
 
@@ -296,3 +306,12 @@ class JaccardDistRBF():
                 return K, K_gradient
         else:
             return K
+
+    def __repr__(self):
+        if self.anisotropic:
+            return "{0}(length_scale=[{1}])".format(
+                self.__class__.__name__, ", ".join(map("{0:.3g}".format,
+                                                   self.length_scale)))
+        else:  # isotropic
+            return "{0}(length_scale={1:.3g})".format(
+                self.__class__.__name__, np.ravel(self.length_scale)[0])
